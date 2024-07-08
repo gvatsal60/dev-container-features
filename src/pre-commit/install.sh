a07a5891609534b0d0c027363455c5aa46af0819
Vatsal Gupta <gvatsal60@gmail.com>
2024-07-08 09:38:39 +0530

    Fixed Pre-commit

--- src/pre-commit/install.sh
+++ src/pre-commit/install.sh
@@ -3,4 +3,7 @@
 # Check if pip, npm, or yarn is installed and install pre-commit
 if command -v "pip3" >/dev/null 2>&1; then
     pip3 install pre-commit
+else
+    echo "Pre-commit installation unsuccessful, aborted!!!"
+    exit 1
 fi
