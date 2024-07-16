bdb7055a8a6682cf10b292e6595d757e3a132e7f
Vatsal Gupta <gvatsal60@gmail.com>
2024-07-16 16:23:17 +0530

    Added global Test

--- test/pre-commit/test.sh
+++ test/pre-commit/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ##########################################################################################
 # File: test
@@ -27,9 +27,13 @@
 
 set -e
 
+# Optional: Import test library bundled with the devcontainer CLI
 # shellcheck source=/dev/null
 . dev-container-features-test-lib
 
+# The 'check' command comes from the dev-container-features-test-lib.
 check "pre-commit --version" pre-commit --version
 
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
 reportResults
