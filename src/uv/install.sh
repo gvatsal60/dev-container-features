3833bc1fb4f7425732dfec13cc429c63d2961459
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2025-09-06 12:22:40 +0000

    Fix syntax errors

--- src/uv/install.sh
+++ src/uv/install.sh
@@ -27,19 +27,19 @@ UV_GIT_URL="https://github.com/astral-sh/uv"
 # Check if curl command is available
 if command -v curl >/dev/null 2>&1; then
   curl -LsSf ${UV_GIT_URL}/install.sh | sh
-else if command -v wget >/dev/null 2>&1; then
+elif command -v wget >/dev/null 2>&1; then
   wget -qO- ${UV_GIT_URL}/install.sh | sh
-else if command -v pipx >/dev/null 2>&1; then
+elif command -v pipx >/dev/null 2>&1; then
   pipx install uv
-else if command -v pip >/dev/null 2>&1; then
+elif command -v pip >/dev/null 2>&1; then
   python3 -m pip install uv
-else if command -v brew >/dev/null 2>&1; then
+elif command -v brew >/dev/null 2>&1; then
   brew install uv
-else if command -v winget >/dev/null 2>&1; then
+elif command -v winget >/dev/null 2>&1; then
   winget install --id=astral-sh.uv  -e
-else if command -v scoop >/dev/null 2>&1; then
+elif command -v scoop >/dev/null 2>&1; then
   scoop install uv
-else if command -v cargo >/dev/null 2>&1; then
+elif command -v cargo >/dev/null 2>&1; then
   cargo install --git ${UV_GIT_URL} uv
 else
     echo "uv installation unsuccessful, aborted!!!"
