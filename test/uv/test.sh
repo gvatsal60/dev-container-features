d83872c7cc3d679c844247da3ae157c08120b79b
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2025-09-08 16:55:18 +0000

    uv test fix

--- test/uv/test.sh
+++ test/uv/test.sh
@@ -32,7 +32,7 @@ set -e
 . dev-container-features-test-lib
 
 # The 'check' command comes from the dev-container-features-test-lib.
-check "check uv version" /root/.local/bin/uv --version
+check "check uv version" export PATH="/root/.local/bin:${PATH}" && uv --version
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
